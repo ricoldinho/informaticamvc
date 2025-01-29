aa62e404895ed645c4fa782b1bf92ecc3fc3c4f4
Manuel Rico <manuelricolopez@liceolapaz.net>
2025-01-29 12:37:52 +0100

    Proyecto creado

---
+++ src/main/java/edu/rico/alumnos/mvc/InformaticaApplication.java
@@ -0,0 +1,13 @@
+package edu.rico.alumnos.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InformaticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InformaticaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/rico/alumnos/mvc/InformaticaApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.rico.alumnos.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InformaticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
